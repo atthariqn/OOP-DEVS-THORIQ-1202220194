d63e1cff4edbf8abd56a08ab995bd245db4cdb89
atthariqn <148106340+atthariqn@users.noreply.github.com>
2023-12-20 08:09:52 +0700

    Add files via upload

---
+++ MODUL3_THOR/src/Bioskop.java
@@ -0,0 +1,21 @@
+public class Bioskop {
+    
+    // TO DO: Create Private Attributes of rows and assign rows to 5
+    
+    // TO DO: Create Private Attributes of seats per rows and assign seats per rows to 10
+    
+    // TO DO: Create 2 dimensional array to store seat reservation status
+    
+    // TO DO:  Add a constructor to initialize multiple chairs
+    public Bioskop() {
+       
+    }
+    //  TO DO: Add a method to display the seat layout
+    public void displaySeating() {
+
+    }
+    //  TO DO: Add a method to reserve seats
+    public void bookSeat(int row, int seat) {
+        
+    }
+}
---
+++ MODUL3_THOR/src/User.java
@@ -0,0 +1,34 @@
+public class User {
+   
+   
+   // TO DO: Create Private Attributes of User (nama, noHandphone)
+   
+   // TO DO: Create Constructor of User
+   
+   public void setName(String nama) {
+         
+      }
+
+   public void setNoHandPhone(String phoneNumber) {
+      }
+   // TO DO: Create register Method to show information about name and phone number
+   public void register(){
+        
+    }
+   // TO DO: Create method to return name and phone number
+   
+   public String getName() {
+     
+  }
+
+   public String getnoHandPhone() {
+     
+  }
+}
+
+
+
+
+
+
+  
